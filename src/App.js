9ff08ec44a4253603291fac2e4bdc63759720029
apierro <antonio.pierro@gmail.com>
2020-06-10 09:43:03 +0200

    adding page structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="header">BPER Logo</div>
+            <div className="main">
+                <div className="card dashboard">dashboard</div>
+                <div className="card chatbox">chatbox</div>
+            </div>
+            <div className="footer">
+                <div className="icon"></div>
+                <div className="icon"></div>
+                <div className="icon"></div>
+                <div className="icon"></div>
+                <div className="icon"></div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
