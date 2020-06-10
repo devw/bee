7130268670d9dabce6146b0d94696d3f8fd82677
apierro <antonio.pierro@gmail.com>
2020-06-10 09:49:54 +0200

    adding chatbox component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Chatbox } from "./components/chatbox";
 
 function App() {
     return (
@@ -8,7 +8,7 @@ function App() {
             <div className="header">BPER Logo</div>
             <div className="main">
                 <div className="card dashboard">dashboard</div>
-                <div className="card chatbox">chatbox</div>
+                <Chatbox></Chatbox>
             </div>
             <div className="footer">
                 <div className="icon"></div>
---
+++ src/components/chatbox.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./chatbox.module.css";
+
+export const Chatbox = () => (
+    <div>
+        <div className={classes.chatbox}>Chatbox</div>
+    </div>
+);
